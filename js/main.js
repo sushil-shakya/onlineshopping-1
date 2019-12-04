b3b4153e5fbd866854b4e968934b2cc4aa630d33
sushil-shakya <shakyas123@gmail.com>
2019-12-04 18:49:43 +0545

    ecommerce frontend design

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// Tooltip
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+// Owl slider
+$('.banner-texts-slider').owlCarousel({
+	items:2,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:false,
+            loop:true
+        }
+    }
+})
+
+// Price Slider
+$( function() {
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 0,
+    max: 500,
+    values: [ 75, 300 ],
+    slide: function( event, ui ) {
+      $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+    }
+  });
+  $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+    " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+} );
+
+
+
